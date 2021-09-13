3366dc2c15a20c263e6e865186df86897d5d494f
chlthr1222@naver.com <60427387+ssook1222@users.noreply.github.com>
2021-09-13 16:37:52 +0900

    UPDATE : 위치 변경

---
+++ school/firstWeek/weather.R
@@ -0,0 +1,10 @@
+weather<-read.csv("./school/data/weather.csv",fileEncoding ="euc-kr",stringsAsFactors = F)
+
+str(weather)
+
+weather$일시<-as.Date(weather$일시)
+weather$요일.구분<-as.factor(weather$요일.구분)
+str(weather)
+
+요일<-weekdays(weather$일시)
+요일<-as.factor(요일)
