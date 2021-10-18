b531287adc56b2b2d7b36df335716aa084a75306
ssook1222 <chlthr1222@naver.com>
2021-10-19 00:02:51 +0900

    UPDATE : 회귀분석 데이터 전처리 추가

--- school/sixthWeek/경데분/regression01.R
+++ school/sixthWeek/경데분/regression01.R
@@ -33,7 +33,8 @@ table(lm_new$payment==9999) #9999로 측정된 값 개수 확인 (155개)
 table(lm_new$satisfaction==9999) #9999로 측정된 값 개수 확인 (155개)
 
 lm_new$payment<-ifelse(lm_new$payment==9999,NA,lm_new$payment) #9999가 값인 경우 NA로 처리
-lm_new$satisfaction<-ifelse(lm_new$satisfaction==9999,NA,lm_new$payment) #9999가 값인 경우 NA로 처리
+#문자형으로 하니 문제4 실행 후 문제가 발생하지 않았음.
+lm_new$satisfaction<-ifelse(lm_new$satisfaction==9999,NA,lm_new$satisfaction) #9999가 값인 경우 NA로 처리
 
 #결측치로 변경되었는지 확인
 table(is.na(lm_new$payment))
@@ -43,7 +44,8 @@ table(is.na(lm_new$satisfaction))
 #바뀐 측정값을 원래 측정값으로 되돌리고 척도를 다시 변경
 
 table(lm_new$payment)
-# factor가 int 형으로 변경되며 이전 factor로 적용된 숫자가 적용된 문제가 발생
+# factor가 int 형으로 변경되어 이전 factor로 적용된 레벨로 적용된 문제가 발생
+# 9999라는 숫자로 해줬기 때문인 것으로 보임.
 
 # 바뀐 측정값을 원래 측정값으로 되돌림
 lm_new$payment<-ifelse(lm_new$payment==2, "간편결제", ifelse(lm_new$payment==3,"계좌이체","신용카드"))
@@ -80,10 +82,32 @@ ggplot(lm_new,aes(1,return))+geom_boxplot()+geom_boxplot(outlier.color = "red")+
 ggplot(lm_new,aes(1,expense))+geom_boxplot()+geom_boxplot(outlier.color = "red")+
   stat_summary(fun="mean", geom="point")
 
-lm_new%>% filter(expense>100000)
+lm_new%>% filter(expense>100000) #case 5개
+lm_new<-lm_new%>%filter(expense<=100000)
 
+#문제 8. 방문회수 대비 구매회수 비율을 나타내는 새로운 변수 rate를 만들고, 이 값이 NaN으로 나온 case에 대해 검토하시오.
+lm_new <- lm_new%>% mutate(rate=order/visit)
+summary(lm_new$rate)
+#NaN은 결측치가 아니라 값을 모르는 것을 의미. (예: 0/0)
+#아래는 예시
+lm_new%>%filter(is.nan(rate))%>%select(rate,order,visit)
 
+#문제 9. 가입경로에 따른 주문회수 평균, 결제수단에 따른 구매금액 평균, 성별에 따른 만족도 평균을 비교하시오.
+lm_new%>%group_by(path)%>%summarise(mean(order))
+as.data.frame(lm_new%>%group_by(payment)%>%summarise(mean(expense))) #변수(열)에 NA가 있는 거라 na.rm=T를 해도 사라지지 않음.
+lm_new%>%group_by(gender)%>%summarise(mean(satisfaction,na.rm=T))
 
+# 문제 10. 한 번이라도 주문한 고객들만으로 lm_order라는 데이터 프레임을 만들고 rate 변수를 삭제한 후, 
+# 변수의 배열 순서를 case, ID, visit, duration, order, moring, expense, return,
+# satisfaction, age, path, payment, gender, os로 변경하시오.
+lm_order <- lm_new %>% filter(order>0)
+lm_order$rate<-NULL
+summary(lm_order)
 
-
+lm_order$payment <- as.factor(lm_order$payment)
+str(lm_order)
+lm_order<-lm_order%>%relocate(os, .after=satisfaction)
+lm_order<-lm_order%>%relocate(gender, .after=satisfaction)
+lm_order<-lm_order%>%relocate(payment, .after=satisfaction)
+lm_order<-lm_order%>%relocate(path, .after=satisfaction)
 
