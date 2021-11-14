e06d94d7d59e859b70b790fb2bdfd7194bc66ae9
ssook <60427387+ssook1222@users.noreply.github.com>
2021-11-14 22:46:57 +0900

    Update sangga.r

--- school/sangga.r
+++ school/sangga.r
@@ -4,6 +4,7 @@ install.packages("raster")
 install.packages("rgeos")
 install.packages("maptools")
 install.packages("rgdal")
+install.packages("doBy")
 
 library(ggmap)
 library(ggplot2)
@@ -12,6 +13,7 @@ library(rgeos)
 library(maptools)
 library(rgdal)
 library(dplyr)
+library(doBy)
 
 sangga<-read.csv("./school/team_assignment/visualization/data/sangga.csv",stringsAsFactors = F)
 seoul_id<-read.csv("./school/team_assignment/visualization/data/seoul_id.csv",stringsAsFactors = F)
@@ -24,15 +26,35 @@ new_map <- fortify(map, region = 'SIG_CD')
 new_map$id <- as.numeric(new_map$id)
 seoul_map <- new_map[new_map$id <= 11740,]
 
-sangga_real<-sangga_new[, c(5,11,38,39,40)]
+sangga_real<-sangga_new[, c(5,11,15,38,39,40)]
 
 
-sangga_test<-sangga_real[c(1:120), ]
-sangga_merge <- left_join(seoul_map, sangga_test, by='id')
+sangga_test<-sangga_real[sample(nrow(sangga_real),120), ]
+sangga_test_new <- sampleBy(~1, frac=.01, data=sangga_real, systematic=TRUE)
+
+
+sangga_merge <- left_join(seoul_map, sangga_test_new, by='id')
 str(sangga_merge)
-sangga_merge$표준산업분류명 <- as.factor(sangga_merge$표준산업분류명)
-ggplot() + geom_polygon(data=sangga_merge, aes(x=long, y=lat, group=group), fill = 'white', color='black')
-ggplot() + geom_polygon(data=sangga_merge, aes(x=long, y=lat, group=group, fill = 상권업종대분류명), color='black')+
-  theme(text=element_text(size = 12,family = "NanumGothic"))
+
+#지역구별 표준산업분류명
+sangga_merge$상권업종대분류명 <- as.factor(sangga_merge$상권업종대분류명)
+
+ggplot() + labs(title="서울 지역구별 가장 많은 상권업종")+
+  geom_polygon(data=sangga_merge, aes(x=long, y=lat, group=group, fill = 상권업종대분류명), color='white')+
+  theme(text=element_text(size = 10,family = "NanumGothic"),
+        plot.title=element_text(size = 14,family = "NanumGothic",face="bold",hjust = 0.5))
 
 
+# 지역구 별 가게 개수
+sangga_test2<- sangga_test_new%>%group_by(시군구명)%>%summarise(count=n())
+seoul_id <- seoul_id%>%arrange(시군구명)
+
+sangga_test2 <- cbind(sangga_test2,seoul_id)
+sangga_test2<-sangga_test2[, c(2:4)]
+sangga_merge2 <- left_join(seoul_map, sangga_test2, by='id')
+
+ggplot()+
+  labs(title="서울 지역구별 상가 수")+
+  geom_polygon(data=sangga_merge2, aes(x=long, y=lat, group=group, fill = count), color='steelblue4')+
+  theme(text=element_text(size = 10,family = "NanumGothic"),
+        plot.title=element_text(size = 15,family = "NanumGothic",face="bold",hjust = 0.5))
